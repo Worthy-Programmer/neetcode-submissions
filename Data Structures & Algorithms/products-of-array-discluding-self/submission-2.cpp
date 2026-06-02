956fcaa7e7acd098f6038857c756ba331bac9cbc
Fahd Fawas <79916616+Worthy-Programmer@users.noreply.github.com>
2026-06-02 13:44:58 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cpp
@@ -0,0 +1,25 @@
+// Using Prefix Suffix solution
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // Two - pass solution
+        vector<int> prefix(size(nums), 1);
+        vector<int> suffix(size(nums) , 1);
+
+        for (size_t i =1; i < size(nums); i++ ) {
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        for (size_t i = size(nums) - 2; i > 0; --i) {
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        suffix[0] = suffix[1] * nums[1];
+
+        vector<int> res(size(nums));
+        for (size_t i = 0; i < size(nums); i++) {
+            res[i] = prefix[i] * suffix[i];
+        }
+
+        return res;
+    }
+};
